261e78553ea40b753387f8002b3595ad8ef19a48
Ronan <galv.ronan@gmail.com>
2021-01-20 18:34:19 -0500

    serve.js completed

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 
+const fs = require('fs');
 const path = require('path');
 
 // instantiate the server
@@ -16,6 +17,72 @@ app.use('/', htmlRoutes);
 
 // allows front-end resources like images, client-side JS, or CSS available when called in index.html
 app.use(express.static('public'));
+
+// consts and functions for apiRoutes start
+const { db } = require('./db/db.json');
+const { v4: uuidv4 } = require('uuid')
+
+// get for db.json and return
+app.get('/api/notes', (req, res) => {
+    return res.json(db);
+});
+
+// note function
+function createNewNote(body, notesArray) {
+    const note = body;
+    // then push the new note to db.json array
+    notesArray.push(note);
+    // write to db.json
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
+    return note;
+}
+
+app.post('/api/notes', (req, res) => {
+    // generate unique id
+    const uniqueId = uuidv4();
+    // assign the unique id to note (req.body)
+    req.body.id = uniqueId;
+    // create note and return it
+    const note = createNewNote(req.body, db);
+    return res.json(note)
+})
+
+function deleteNote(searchedId, notesArray) {
+    // if searched id is in json, continue
+    if (notesArray.some(note => note.id === searchedId)) {
+        // Acquire the index of searchedId
+        const noteIndex = notesArray.findIndex(note => note.id === searchedId);
+        notesArray.splice(noteIndex, 1);
+        // Finally, write the db.json with deleted note
+        fs.writeFileSync(
+            path.join(__dirname, './db/db.json'),
+            JSON.stringify({ notes: notesArray }, null, 2)
+        );
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// delete from db.json
+app.delete('api/notes/:id', (req,res) => {
+    // var for id searched
+    const searchedId = req.params.id;
+    // get index of object with id searched
+    const response = deleteNote(searchedId, notes);
+
+    // see if id is in notes then delete else send error
+    if (response) {
+        return res.status(200).send(`Note with ID: ${searchedId} deleted from database`)
+    } else {
+        return res.status(400).send("Enter a valid note id to delete");
+    }
+});
+//consts for apiRoutes end
+
 
 // api routes start
 
